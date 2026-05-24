be1b359548af6e5051f6a2cae464bec0b53e00c5
Kaushik <kaushik.lf07@gmail.com>
2026-05-24 22:21:37 +0530

    client ENV for backend URL

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL:
-        "http://localhost:4000/api",
+        process.env.NEXT_PUBLIC_BACKEND_URL,
 
     withCredentials: true,
 
